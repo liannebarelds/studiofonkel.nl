e2a48d4e799e01dd06385f2c60ee9bd7f3f61eb3
dbeeke <daniel@studiofonkel.nl>
2015-02-24 11:33:15 +0100

    adding blockquote

--- app/_plugins/detailimage.rb
+++ app/_plugins/detailimage.rb
@@ -3,14 +3,14 @@ module Jekyll
 
     def initialize(tag_name, text, tokens)
 
-      @params = text.split(' ')
+      @params = text.split(' "')
       @url = @params[0]
-      @description = @params[0]
+      @description = @params[1].split('"')[0]
 
     end
 
     def render(context)
-      "<div class='detailimage'><img src='#{@url}'><div class='detailimage-description'></div></div>"
+      "<div class='detailimage'><img src='#{@url}'><div class='detailimage-description'>#{@description}</div></div>"
     end
   end
 end
