cca2dba9f4f068bb0500f54a53c0b4eaebffddad
dbeeke <daniel@studiofonkel.nl>
2015-02-25 11:06:36 +0100

    restructure layouts

--- app/_pages/over-ons.md
+++ app/_pages/over-ons.md
@@ -1,8 +1,41 @@
 ---
 title: "Studio Fonkel"
 subtitle: "Gewone jongens die mooie dingen maken"
-background: over-ons.jpg
+header: over-ons.jpg
 menu-weight: 1
+introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor."
 ---
-{% include misc/teamwidget.html %}
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.
+
+{% include misc/teamwidget.html %}
+
+<ol class="convinceblock">
+  <li class="convinceblock-item">
+    <div class="convinceblock-item-content">
+      <h3 class="convinceblock-item-title">We are the best</h3>
+      <p class="convinceblock-item-text">Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
+    </div>
+    {% picture convince carto1.png %}
+  </li>
+  <li class="convinceblock-item">
+    <div class="convinceblock-item-content">
+      <h3 class="convinceblock-item-title">We are the best</h3>
+      <p class="convinceblock-item-text">Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
+    </div>
+    {% picture convince carto1.png %}
+  </li>
+  <li class="convinceblock-item">
+    <div class="convinceblock-item-content">
+      <h3 class="convinceblock-item-title">We are the best</h3>
+      <p class="convinceblock-item-text">Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
+    </div>
+    {% picture convince carto1.png %}
+  </li>
+  <li class="convinceblock-item">
+    <div class="convinceblock-item-content">
+      <h3 class="convinceblock-item-title">We are the best</h3>
+      <p class="convinceblock-item-text">Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>
+    </div>
+    {% picture convince carto1.png %}
+  </li>
+</ol>
--- app/js/teamwidget.js
+++ app/js/teamwidget.js
@@ -1,38 +1,29 @@
 $(function() {
   'use strict'
 
-  function updateSelects(axis) {
-    if (axis == 'x') {
-      $('#teamwidget-selectY option').css('display', 'block')
-      $('#teamwidget-selectY option[value="'+ $('#teamwidget-selectX option:selected')[0].value + '"]').css('display', 'none')
-    }
+  function updateSelects() {
+    $('.teamwidget-select option').css('display', 'block')
 
-    else {
-      $('#teamwidget-selectX option').css('display', 'block')
-      $('#teamwidget-selectX option[value="'+ $('#teamwidget-selectY option:selected')[0].value + '"]').css('display', 'none')
-    }
+    var xValue = $('#teamwidget-selectX option:selected').val()
+    var yValue = $('#teamwidget-selectY option:selected').val()
+
+    $('#teamwidget-selectY option[value="'+ xValue + '"]').css('display', 'none')
+    $('#teamwidget-selectX option[value="'+ yValue + '"]').css('display', 'none')
   }
 
-  function updatePositions(characteristic, axis) {
-    console.log('hahaha')
-    var other_axis
-    axis == 'x' ? other_axis = 'y' : other_axis = 'x'
-    var other_axis_characteristic = $('#teamwidget-select' + other_axis.toUpperCase() + ' option:selected')[0].value,
-        widget_width = $('.teamwidget').outerWidth()
+  function updatePositions() {
 
-    $.each(window.teaminfo, function(i, object) {
-      var key = object.key,
-          this_axis_value = object.preferences[characteristic],
-          other_axis_value = object.preferences[other_axis_characteristic],
-          translateString
 
-      if (axis == 'x') {
-        translateString = 'translate(' + (this_axis_value * widget_width / 100) + 'px, ' + (other_axis_value * widget_width / 100) + 'px)'
-      }
+    $.each(window.teaminfo, function(delta, object) {
+      var selected = [],
+      widget_width = $('.teamwidget').outerWidth(),
+      key = object.key
 
-      else {
-        translateString = 'translate(' + (other_axis_value * widget_width / 100) + 'px, ' + (this_axis_value * widget_width / 100) + 'px)'
-      }
+      $.each($('.teamwidget-select option:selected'), function (selectDelta, select) {
+        selected[selectDelta] = object.preferences[$(select).val()]
+      })
+
+      var translateString = 'translate(' + (selected[0] * widget_width / 100) + 'px, ' + (selected[1] * widget_width / 100) + 'px)'
 
       $('#teamwidget-member-member-' + key).css({
         transform: translateString,
@@ -43,50 +34,34 @@ $(function() {
     })
   }
 
-  function detailsOnMember(item) {
-    item.addClass('teamwidget-member-active')
-    var thisPosition = item.offset()
-    var axesPosition = $('.axes').offset()
-    var translateString = 'translate(' + (axesPosition.left - thisPosition.left) + 'px, ' + (axesPosition.top - thisPosition.top) + 'px) translate(4vmin, 4vmin)'
-    
-    item.find('> .teamwidget-member-icon-wrapper').css({
-      transform: translateString,
-        MozTransform: translateString,
-        WebkitTransform: translateString,
-        msTransform: translateString
-    })
-  }
+  // function detailsOnMember(item) {
+  //   item.addClass('teamwidget-member-active')
+  //   var thisPosition = item.offset()
+  //   var axesPosition = $('.axes').offset()
+  //   var translateString = 'translate(' + (axesPosition.left - thisPosition.left) + 'px, ' + (axesPosition.top - thisPosition.top) + 'px) translate(4vmin, 4vmin)'
 
-  updateSelects('x')
-  updateSelects('y')
-  updatePositions($('#teamwidget-selectY option:selected')[0].value, 'y')
+  //   item.find('> .teamwidget-member-icon-wrapper').css({
+  //     transform: translateString,
+  //       MozTransform: translateString,
+  //       WebkitTransform: translateString,
+  //       msTransform: translateString
+  //   })
+  // }
 
   // Event handlers
-  $('.teamwidget').on('change', 'select', function(e) {
-    var select_id = $(this).attr('id')
-    var axis = select_id.charAt(select_id.length - 1).toLowerCase()
-    updateSelects(axis)
-    updatePositions($('#' + select_id + ' option:selected')[0].value, axis)
+  $('.teamwidget-select').on('change', function(e) {
+    updateSelects()
+    updatePositions()
   })
 
-  $('.teamwidget-members').on('click touch', '.teamwidget-member', function() {
-    detailsOnMember($(this), false)
-  })
+  // $('.teamwidget-members').on('click touch', '.teamwidget-member', function() {
+  //   detailsOnMember($(this), false)
+  // })
 
-  var timeout
   $(window).resize(function() {
-    $('body').addClass('teamwidget-no-transitions')
-
-    clearTimeout(timeout)
-    timeout = setTimeout(function(){
-      updatePositions($('#teamwidget-selectX option:selected')[0].value, 'x')
-      
-      if ($('.teamwidget-member-active').length > 0) {
-        detailsOnMember($('.teamwidget-member-active'))
-      }
-      
-      $('body').removeClass('teamwidget-no-transitions')
-    }, 400)
+    updatePositions()
   })
+
+  updatePositions()
 
 });
