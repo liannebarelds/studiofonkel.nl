d0d95ea8d95c124a481568b215bea7a985c2516f
Laurens Bruijn <laurens@LB-MBP.fritz.box>
2015-02-26 12:23:18 +0100

    parallax fixed!

--- app/js/imageparallax.jquery.js
+++ app/js/imageparallax.jquery.js
@@ -1,80 +1,91 @@
-
 ;(function ( $, window, document, undefined ) {
 
-    // Create the defaults once
-    var pluginName = "imageparallax"
+  // Create the defaults once
+  var pluginName = "imageparallax"
 
-    // The actual plugin constructor
-    function Plugin( element, options ) {
-        this.element = element;
-        this._name = pluginName;
+  // The actual plugin constructor
+  function Plugin( element, options ) {
+    this.element = element;
+    this._name = pluginName;
 
-        this.init()
-    }
+    this.init()
+  }
 
-    Plugin.prototype = {
+  Plugin.prototype = {
 
-        init: function() {
-          var that = this
-          setTimeout(function () {
+    init: function() {
+      var that = this
+      that.updateDelay;
+      that.element = $('.image-inner', that.element)
+      that.image = $('img', that.element)
+      that.ticking = false
+      that.windowHeight = window.innerHeight
+      that.update(that)
 
-            that.scrollImageHeight = $(that.element).innerHeight()
-            that.image = $('img', that.element)
-            that.ticking = false
-
-            that.scrollImageMin = $(that.element).offset().top
-            that.scrollImageMax = that.scrollImageMin + that.scrollImageHeight
-            that.windowHeight = window.innerHeight
-
-            $(window).on('scroll', function () {
-              if (that.ticking === false) {
-                that.ticking = true
-                requestAnimationFrame(function () {
-                  that.scroll(that)
-                })
-              }
-            })
-
+      $(window).on('scroll', function () {
+        if (that.ticking === false) {
+          that.ticking = true
+          requestAnimationFrame(function () {
             that.scroll(that)
-          }, 300)
-
-        },
-
-        scroll: function(that) {
-          if (!that.imageHeight) {
-            that.imageHeight = that.image.innerHeight()
-          }
-
-          var scrollPercentage = (window.scrollY - (that.scrollImageMin - that.windowHeight)) / (that.scrollImageMax - (that.scrollImageMin - that.windowHeight))
-
-          if (that.scrollImageMax > window.scrollY && that.scrollImageMin < (that.windowHeight + window.scrollY)) {
-            var translateY = Math.floor((that.imageHeight + that.scrollImageHeight) * scrollPercentage * -1 / 2)
-            that.image.css( "transform", "translate(0, " + translateY + "px)" );
-
-            console.log('--AAN--');
-          }
-            console.log('---------');
-            console.log('that.scrollImageMax: ' + that.scrollImageMax);
-            console.log('that.scrollImageMin: ' + that.scrollImageMin);
-            console.log('that.windowHeight: ' + that.windowHeight);
-            console.log('that.imageHeight: ' + that.imageHeight);
-            console.log('that.scrollImageHeight: ' + that.scrollImageHeight);
-            console.log('scrollPercentage: ' + scrollPercentage);
-            console.log('window.scrollY: ' + window.scrollY);
-
-          that.ticking = false
+          })
         }
-    };
+      })
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName,
-                new Plugin( this, options ));
-            }
-        });
-    };
+      $(window).resize(function() {
+        clearTimeout(that.updateDelay);
+        that.updateDelay = setTimeout(
+          function() {
+            that.update(that)
+            that.scroll(that)
+          }, 100);
+      })
+
+      setTimeout(function () {
+        that.update(that)
+        that.scroll(that)
+      }, 1000)
+
+    },
+
+    update: function(that) {
+      $(that.element).innerHeight($(that.element).innerWidth()*0.5625)
+      that.scrollImageHeight = $(that.element).innerHeight()
+      that.scrollImageMin = $(that.element).offset().top
+      that.scrollImageMax = that.scrollImageMin + that.scrollImageHeight
+      that.imageHeight = that.image.innerHeight()
+    },
+
+    scroll: function(that) {
+      var scrollPercentage = (window.scrollY - (that.scrollImageMin - that.windowHeight)) / (that.scrollImageMax - (that.scrollImageMin - that.windowHeight))
+
+      if (that.scrollImageMax > window.scrollY && that.scrollImageMin < (that.windowHeight + window.scrollY)) {
+        var translateY = Math.floor((that.imageHeight - that.scrollImageHeight) * scrollPercentage * -1)
+        that.image.css( "transform", "translate(0, " + translateY + "px)" );
+
+        // console.log('--AAN--');
+      }
+      // console.log('---------');
+      // console.log('that.scrollImageMax: ' + that.scrollImageMax);
+      // console.log('that.scrollImageMin: ' + that.scrollImageMin);
+      // console.log('that.windowHeight: ' + that.windowHeight);
+      // console.log('that.imageHeight: ' + that.imageHeight);
+      // console.log('that.scrollImageHeight: ' + that.scrollImageHeight);
+      // console.log('scrollPercentage: ' + scrollPercentage);
+      // console.log('window.scrollY: ' + window.scrollY);
+
+      that.ticking = false
+    }
+  };
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName,
+        new Plugin( this, options ));
+      }
+    });
+  };
 
 })( jQuery, window, document );
